a1497bb04c7244fbf2e02515bfb504a4637ce186
BuildTools <unconfigured@null.spigotmc.org>
2024-01-03 21:49:38 +0100

    File Iniziale

---
+++ src/main/java/me/abu/enchantplus/EnchantPlus.java
@@ -0,0 +1,17 @@
+package me.abu.enchantplus;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EnchantPlus extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
